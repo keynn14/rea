a35d43ee3a3f6dae40afbfe441687b09ce2acf62
Kane <k.restauro.543752@umindanao.edu.ph>
2026-09-20 11:29:12 +0800

    Initial commit: Rea Rosal cute & elegant profile website

---
+++ rea/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using rea.Models;
+using System.Diagnostics;
+
+namespace rea.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
